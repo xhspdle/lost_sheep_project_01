401837d03b9cb5d1eaddf3b7c4209d95928f4eef
DevLDK <xhspdle@gmail.com>
2021-01-11 20:20:32 +0900

    learn complete

---
+++ src/routes/Question.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Question.css";
+
+function Question(props) {
+    console.log(props)
+    return (
+        <div className="question__container">
+            <span>
+                호롤로롤로로로로로로로로로로로로
+            </span>
+            <span>호롤로로로</span>
+        </div>
+    );
+}
+
+export default Question;
