ae6fd559af2aca3f47d52723016b039c0b583fb5
DevLDK <xhspdle@gmail.com>
2021-03-04 21:19:51 +0900

    gtm, ga, adfit inserted

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React from "react";
 import { HashRouter, Route } from "react-router-dom";
+import TagManager from "react-gtm-module";
 import Home from "./routes/Home";
 import Question from "./routes/Question";
 import Result from "./routes/Result";
 import "./App.css";
-import imgBack from "./routes/background.png";
+import imgBack from "./routes/background2.png";
 
 const App = () => {
+  const tagManagerArgs = {
+    gtmId: 'GTM-MQ6FT8Q'
+  }
+  TagManager.initialize(tagManagerArgs);
   return (
     <div className="App">
       <div className="App-header">
         <div className="App-body"
-          style={{backgroundImage: `url(${imgBack})`,
+          style={{/*backgroundImage: `url(${imgBack})`,
           backgroundSize: `cover`,
-          backgroundPosition: `center`}}
+  backgroundPosition: `center`*/}}
         >
+          <ins className="kakao_ad_area" 
+               style={{
+                 display: 'none'   
+               }} 
+          data-ad-unit    = "DAN-LTwgeATAMnreP81l" 
+          data-ad-width   = "320" 
+          data-ad-height  = "100"></ins> 
+          <img className="App-back-img" src={imgBack} alt="sheep and mirror"/>
           <HashRouter>
             <Route path="/" exact={true} component={Home} />
             <Route path="/question" component={Question} />
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
-import imgSheep from "./main.png";
+import imgSheep from "./main2.png";
 
 
 function Home() {
